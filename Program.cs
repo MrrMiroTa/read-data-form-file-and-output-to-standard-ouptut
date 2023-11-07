2f4e4a898e5b0d2261b6def78a4eefd316acafcb
MrrMiroTa <142069655+MrrMiroTa@users.noreply.github.com>
2023-11-07 21:25:05 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Read_data_from_file_and_filter_name_change_value_M_F
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    namespace Read_data_from_file
+    {
+        public class Person
+        {
+            //instance field
+            protected string _name = "";
+            protected string _gender = "";
+            protected byte _age = 0;
+
+            //instance method
+            public string GetName() { return _name; }
+            public string GetGender() { return _gender; }
+            public byte GetAge() => _age;
+
+            public bool SetData(string data, string delimiter = "/")
+            {
+                string[] arr = data.Split(delimiter);
+                if (arr.Length < 3) return false;
+                string name = arr[0].Trim();
+                string gender = arr[1].Trim();
+                if (byte.TryParse(arr[2], out byte age) == false) return false;
+                _name = name;
+                _gender = gender;
+                _age = age;
+                return true;
+            }
+
+            public string GetInfo()
+            {
+                string nameInfo = $"{_name,-30}";
+                string genderInfo = $"{_gender,-6}";
+                string ageInfo = $"{_age,4}";
+                return $"{nameInfo} {genderInfo} {ageInfo}";
+            }
+        }
+    }
+}
---
+++ data_sort.cs
@@ -0,0 +1,57 @@
+﻿using Read_data_from_file_and_filter_name_change_value_M_F.Read_data_from_file;
+
+string filename = "Personal_file.txt";
+Console.WriteLine($"Reading People Data from file {filename}");
+Console.SetIn(File.OpenText(filename));
+
+int line = 1;
+List<Person> people = new List<Person>();
+
+while (true)
+{
+    Console.WriteLine($"Line {line++}...\n");
+    string? data = Console.ReadLine();
+    if (string.IsNullOrEmpty(data))
+    {
+        Console.WriteLine("Nothing");
+        break;
+    }
+    Person person = new Person();
+    if (person.SetData(data, "/") == false)
+    {
+        Console.WriteLine("Invalid");
+        continue;
+    }
+    Console.WriteLine("OK\n");
+    people.Add(person);
+}
+
+// Filter persons for age > 25
+var filteredPeople = people.Where(p => p.GetAge() < 25).ToList();
+
+// Sort persons in ascending order by name
+filteredPeople.Sort((p1, p2) => string.Compare(p1.GetName(), p2.GetName()));
+
+// Update values of gender from "Male" to "M" and "Female" to "F"
+foreach (var person in filteredPeople)
+{
+    if (person.GetGender() == "Male")
+    {
+        person.SetData($"{person.GetName()}/M/{person.GetAge()}");
+    }
+    else if (person.GetGender() == "Female")
+    {
+        person.SetData($"{person.GetName()}/F/{person.GetAge()}");
+    }
+}
+
+string result = string.Join("\n", filteredPeople.Select(p => p.GetInfo()));
+
+string heading = $"{"Name",-30}{"Gender",-6}{"Age",4}";
+string bar = new string('_', 45);
+Console.WriteLine();
+Console.WriteLine("People Information\n \n");
+Console.WriteLine(heading);
+Console.WriteLine(bar);
+Console.WriteLine(result);
+Console.WriteLine(bar);
